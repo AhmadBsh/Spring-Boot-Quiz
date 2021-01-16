382df1d6cd6a60f4a6fdd24afae4ebbcddf842fd
AhmadBsh <ahmad.bsh237@gmail.com>
2021-01-16 18:04:20 +0200

    -added backgrounds -worked on ProductManagement page

--- src/views/CManagement.vue
+++ src/views/CManagement.vue
@@ -1,22 +1,37 @@
 <template>
+<v-main class="back fill-height">
+    
   <div class="cmanagement">
+      <v-row>
+      <v-col >
   <h1> This is the Clients Management page </h1>    
+      </v-col >
+       </v-row>
   </div>
+ 
+  </v-main>
 </template>
 
 <script>
-
-
 export default {
   name: 'Home',
   components: {},
 
+  data: () => ({
 
 
-
+  })
 
 }
 
-
-
 </script>
+
+<style >
+
+.back {
+  background-image: url("~@/assets/CLMBackground.jpg");
+  background-size: cover;
+  width: 100%;
+}
+
+</style>
