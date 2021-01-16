1012931e0492924969e739dcc092d69a12dd27bd
AhmadBsh <ahmad.bsh237@gmail.com>
2021-01-16 06:42:52 +0200

    -Starting of the project

---
+++ src/components/SideBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-navigation-drawer color="primary" dark app permanent>
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.link"
+          link
+          exact
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { title: 'Products Management', icon: 'mdi-home', link: { name: 'Community' } },
+      { title: 'Clients Management', icon: 'mdi-chart-bar', link: { name: 'Boards' } },
+      { title: 'Sales', icon: 'mdi-chat'  },
+    ],
+  }),
+}
+</script>
